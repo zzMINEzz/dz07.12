21e8b4fcd5c57d55d78f8977cd89d16ee7165caa
zzMINEzz <125128038+zzMINEzz@users.noreply.github.com>
2024-12-07 14:14:43 +0200

    Add files via upload

---
+++ comments.js
@@ -0,0 +1,61 @@
+let nameInput = document.querySelector('.name__input');
+let btn = document.querySelector('.button');
+let textArea = document.querySelector('.text__btn');
+let commList = document.querySelector('.comments-list');
+
+function limitComments() {
+   if(commList.length > 12) {
+      commList.removeChild(commList.firstChild);
+    }
+  }
+btn.addEventListener('click', function () {
+  let nameInputValue = nameInput.value;
+  localStorage.setItem('input', nameInputValue);
+
+  let textAreaValue = textArea.value;
+  localStorage.setItem('textArea', textAreaValue);
+
+  let divComm = document.createElement('p');
+  divComm.textContent = `${nameInputValue}: ${textAreaValue}`;
+  commList.appendChild(divComm);
+});
+let comments = [
+    "Це чудово!",
+    "Гарна робота!",
+    "Цікавий підхід!",
+    "Мені подобається ця ідея.",
+    "Це вражає!",
+    "Треба подумати над цим.",
+    "Добре зроблено!",
+    "Це виглядає складно.",
+    "Це корисна інформація.",
+    "Дякую за пояснення!",
+    "TOP",
+    "Санечка снимаешь?"
+  ];
+  
+  
+let names = [
+    "Анна",
+    "Петро",
+    "Олена",
+    "Максим",
+    "Вікторія",
+    "Олександр",
+    "Катерина",
+    "Дмитро",
+    "Санечка снимает",
+    "ЯКІЛЬКА",
+    "Профимастер21341"
+  ];
+  function getRandomComment() {
+    let randomName = names[Math.floor(Math.random() * names.length)];
+    let randomComment = comments[Math.floor(Math.random() * comments.length)];
+  
+    let commentReview = document.createElement('p');
+    commentReview.textContent = `${randomName}: ${randomComment}`;
+    commList.appendChild(commentReview);
+  }
+  
+ 
+  setInterval(getRandomComment, 5550);
---
+++ script.js
@@ -0,0 +1,65 @@
+let url = "93cf8b461875a73100e8f300e649a579"
+let weatherDiv = document.querySelector('.weather')
+let weatherInp = document.querySelector('.weather-response')
+let weatherBtn = document.querySelector('.weather-button')
+weatherBtn.addEventListener('click',function(){
+    let weatherInpVal = weatherInp.value
+    let divResult = document.querySelector('.weather-total')
+    divResult.innerHTML = '';
+    let weatherApiKey = "93cf8b461875a73100e8f300e649a579"
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${weatherInpVal}&appid=${weatherApiKey}&units=metric&lang=uk`,{method:"GET"})
+
+    
+    .then(Response =>{
+        if(!Response.ok){
+            console.log(Response)
+        }
+        return Response.json()
+    })
+    .then(data=>{
+        console.log(data)
+        let cityName = document.createElement('h2')
+        cityName.textContent = `City:${data.name}`
+        divResult.appendChild(cityName)
+        let weatherTemp = document.createElement('p')
+        weatherTemp.textContent =`Temp:${Math.floor(data.main.temp)}° `
+        divResult.appendChild(weatherTemp)
+        let country = document.createElement('p')
+        country.textContent = `Country:${data.sys.country}`
+        divResult.appendChild(country)
+        let winds = document.createElement('p')
+        winds.textContent = `wind:${data.wind.speed}`
+        divResult.appendChild(winds)
+        let weathers = document.createElement('p')
+        weathers.textContent = `weather:${data.weather[0].main}`
+        divResult.appendChild(weathers)
+        
+        let weatherIcon = document.createElement('img')
+            weatherIcon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+            weatherIcon.alt = data.weather[0].description
+            divResult.appendChild(weatherIcon)
+    })
+    .catch(error =>{
+        console.error(error)
+    })
+})
+
+// коментари
+// let nameInput = document.querySelector('.name__input')
+// let btn = document.querySelector('.button')
+// let textArea = document.querySelector('.text__btn')
+// let commList = document.querySelector('.comments-list')
+// btn.addEventListener('click', function(){
+// let nameInputValue = nameInput.value
+// localStorage.setItem('input',nameInputValue)
+
+
+// let textAreaValue = textArea.value
+// localStorage.setItem('textArea',textAreaValue)
+// })
+// let nameText = localStorage.getItem('input')
+// let areaText = localStorage.getItem('textArea')
+// let divComm = document.createElement('div')
+// divComm.append(nameText)
+// divComm.append(areaText)
+// commList.appendChild(divComm)
